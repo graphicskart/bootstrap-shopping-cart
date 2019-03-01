aa27b2c1ff1ffbc5da1126dd459be4942b9b3a1f
graphicskart <graphicskart2018@gmail.com>
2019-03-01 18:43:07 +0530

    Updates

---
+++ precache-manifest.36352c0e055a56c81c1da9c545062176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "326a4eb6d2b595f199a847db367832c1",
+    "url": "/bootstrap-shopping-cart/static/media/product1.326a4eb6.jpg"
+  },
+  {
+    "revision": "26b14b14b9f135df6f0e",
+    "url": "/bootstrap-shopping-cart/static/js/runtime~main.26b14b14.js"
+  },
+  {
+    "revision": "1d654651e1fea6979bb4",
+    "url": "/bootstrap-shopping-cart/static/js/main.1d654651.chunk.js"
+  },
+  {
+    "revision": "6a78dd4255c67c4d27c4",
+    "url": "/bootstrap-shopping-cart/static/js/2.6a78dd42.chunk.js"
+  },
+  {
+    "revision": "1d654651e1fea6979bb4",
+    "url": "/bootstrap-shopping-cart/static/css/main.3293590e.chunk.css"
+  },
+  {
+    "revision": "c21efbab83b51230f3e44b527de52942",
+    "url": "/bootstrap-shopping-cart/index.html"
+  }
+];
